5a718535ce0c142cebb1bf23f507c2552d9cd8ab
cgillies93 <59749520+cgillies93@users.noreply.github.com>
2020-08-06 11:51:25 -0700

    first commit

---
+++ src/todos/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import NewTodoForm from './NewTodoForm'
+import TodoListItem from './TodoListItem';
+import { removeTodo, markAsCompleted } from './actions';
+import './TodoList.css';
+
+const TodoList = ( { todos = [], onRemovePressed, onCompletedPressed } ) => (
+    <div className="list-wrapper">
+        <NewTodoForm />
+        {todos.map(todo =>
+          <TodoListItem
+              todo={todo}
+              onRemovePressed={onRemovePressed}
+              onCompletedPressed={onCompletedPressed} />
+        )}
+    </div>
+);
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text)),
+    onCompletedPressed: text => dispatch(markAsCompleted(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
